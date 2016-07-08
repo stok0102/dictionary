7187797c88b50bf1672c14dbda2572b570645995
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-08 11:24:24 -0700

    add routes to app rb

--- app.rb
+++ app.rb
@@ -8,47 +8,43 @@ get('/') do
   erb(:index)
 end
 
-get('/dealerships/new') do
-   erb(:dealerships_form)
+get('/words/new') do
+   erb(:words_form)
 end
 
-get('/dealerships') do
-  @dealerships = Dealership.all()
-  erb(:dealerships)
+get('/words') do
+  @words = Word.all()
+  erb(:words)
 end
 
-post('/dealerships') do
+post('/words') do
   name = params.fetch('name')
-  Dealership.new(name).save()
-  @dealerships = Dealership.all()
+  Word.new(name).save()
+  @words = Word.all()
   erb(:success)
 end
 
-get('/vehicles/:id') do
-  @vehicle = Vehicle.find(params.fetch('id').to_i())
-  erb(:vehicle)
+get('/definitions/:id') do
+  @definition = Definition.find(params.fetch('id').to_i())
+  erb(:definition)
 end
 
-get('/dealerships/:id') do
-  @dealership = Dealership.find(params.fetch('id').to_i())
-  erb(:dealership)
+get('/words/:id') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word)
 end
 
-get('/dealerships/:id/vehicles/new') do
-    @dealership = Dealership.find(params.fetch('id').to_i())
-    erb(:dealership_vehicles_form)
+get('/words/:id/definitions/new') do
+    @word = Word.find(params.fetch('id').to_i())
+    erb(:word_definitions_form)
 end
 
-post('/vehicles') do
-  make = params.fetch('make')
-  model = params.fetch('model')
-  year = params.fetch('year')
-  color = params.fetch('color')
-  engine_size = params.fetch('engine_size')
-  number_of_doors = params.fetch('number_of_doors')
-  @vehicle = Vehicle.new({:make=> make, :model=> model, :year=> year, :color=> color, :engine_size=> engine_size, :number_of_doors=> number_of_doors})
-  @vehicle.save()
-  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
-  @dealership.add_vehicle(@vehicle)
+post('/definitions') do
+  definition = params.fetch('definition')
+  part_of_speech = params.fetch('part_of_speech')
+  @definition = Definition.new({:definition=> definition, :part_of_speech=> part_of_speech})
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
   erb(:success)
 end
