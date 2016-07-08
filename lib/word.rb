f1f174e057e7b1ab35cf0693d31de2cce7d7a66c
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-08 08:44:02 -0700

    create empty array of definitions for word class, create all method for word class

--- lib/word.rb
+++ lib/word.rb
@@ -1,10 +1,37 @@
 class Word
   @@words = []
 
-  attr_reader(:name, :definition)
+  attr_reader(:name, :definition, :id)
 
   define_method(:initialize) do |name|
     @name = name
     @definition = []
+    @id = @@words.length().+(1)
+  end
+
+    define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(id)
+        found_word = word
+      end
+    end
+    found_word
+  end
+
+  define_method(:add_definition) do |definition|
+    @definition.push(definition)
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,9 +4,22 @@ require('word')
 
 describe('Word') do
   describe('#name') do
-    it 'returns the name of the name' do
+    it 'returns the name of the word' do
       test_word = Word.new('gallivant')
       expect(test_word.name()).to(eq('gallivant'))
+    end
+  end
+
+  describe('#definition') do
+    it 'initially returns an empty array' do
+      test_word = Word.new('gallivant')
+      expect(test_word.definition()).to(eq([]))
+    end
+  end
+
+  describe('.all') do
+    it 'initially returns an empty array' do
+      expect(Word.all()).to(eq([]))
     end
   end
 end
