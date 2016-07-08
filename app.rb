258455dc896968bc8cc4d34388333cc020ee50dc
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-08 10:53:07 -0700

    add erbs to views folder for routing

---
+++ app.rb
@@ -0,0 +1,54 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/dealerships/new') do
+   erb(:dealerships_form)
+end
+
+get('/dealerships') do
+  @dealerships = Dealership.all()
+  erb(:dealerships)
+end
+
+post('/dealerships') do
+  name = params.fetch('name')
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
+  erb(:success)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
+  erb(:vehicle)
+end
+
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
+get('/dealerships/:id/vehicles/new') do
+    @dealership = Dealership.find(params.fetch('id').to_i())
+    erb(:dealership_vehicles_form)
+end
+
+post('/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  color = params.fetch('color')
+  engine_size = params.fetch('engine_size')
+  number_of_doors = params.fetch('number_of_doors')
+  @vehicle = Vehicle.new({:make=> make, :model=> model, :year=> year, :color=> color, :engine_size=> engine_size, :number_of_doors=> number_of_doors})
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:success)
+end
