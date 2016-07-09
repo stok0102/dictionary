a5a842bb79680ac0a6b0f566b976b61e0c654f9b
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-08 17:45:00 -0700

    add definitions block to integration spec. definition id page not working

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -8,11 +8,18 @@ set(:show_exceptions, false)
 describe('the add word path', {:type => :feature}) do
   it('takes word and adds to list of words') do
     visit('/')
-    click_link('Add New Word')
-    fill_in('name', :with => 'Happy Motors')
-    click_button('Add Dealership')
+    click_link('Add Word')
+    fill_in('name', :with => 'dropsy')
+    click_button('Add Word')
     click_link('Home')
-    click_link('See Dealership List')
+    click_link('See Words')
+    click_link('dropsy')
+    click_link('Add a new definition')
+    fill_in('meaning', :with=> 'archaic term for edema')
+    fill_in('part_of_speech', :with=> 'noun')
+    click_button('Add definition')
+    click_link('Home')
+    
     expect(page).to have_content('Happy Motors')
   end
 end
