9622da9970ba6cc672c500aa4b27dac638ba8e10
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-08 19:47:05 -0700

    expand integration spec route to pass definition block

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -13,13 +13,17 @@ describe('the add word path', {:type => :feature}) do
     click_button('Add Word')
     click_link('Home')
     click_link('See Words')
+    expect(page).to have_content('dropsy')
     click_link('dropsy')
     click_link('Add a new definition')
     fill_in('meaning', :with=> 'archaic term for edema')
     fill_in('part_of_speech', :with=> 'noun')
-    click_button('Add definition')
+    click_button('Add Definition')
+    expect(page).to have_content('Success!')
     click_link('Home')
-    
-    expect(page).to have_content('Happy Motors')
+    click_link('See Words')
+    click_link('dropsy')
+    expect(page).to have_content('archaic term for edema')
+
   end
 end
